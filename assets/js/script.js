fcf1952b6f798f3f9b2905f0c492ffc8fe5ff2a3
kenya flores <keniaccvc@gmail.com>
2022-01-17 17:35:11 -0600

    cambios guardados de viajes chile uno

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 script.js
+
+$(window).scroll(function(){
+
+    if ($(this).scrollTop() > 400) {
+        $("#menu").removeClass("navbar");
+       $('#menu').addClass("bg-interno");
+      } else {
+    
+       $("#menu").removeClass("bg-interno");
+       $('#menu').addClass("navbar");
+      }
+
+   });
